dc7411a3c1c977a8931e902ac84523bfd6637198
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-04 00:17:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -39,5 +39,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
